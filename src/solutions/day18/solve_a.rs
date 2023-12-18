cb7a80a88231e25dd914f666086f536abf04ce14
Evert van Aart <evert@miro.com>
2023-12-18 17:58:31 +0100

    Day 18 WIP

--- src/solutions/day18/solve_a.rs
+++ src/solutions/day18/solve_a.rs
@@ -1,6 +1,166 @@
+use std::collections::HashSet;
+
 use crate::solutions::Solution;
 
+enum Direction {
+    North,
+    East,
+    South,
+    West
+}
+
+impl Direction {
+    fn parse(input: &str) -> Direction {
+        match input {
+            "R" => Direction::East,
+            "L" => Direction::West,
+            "U" => Direction::North,
+            "D" => Direction::South,
+            _ => panic!()
+        }
+    }
+}
+
+struct Instruction {
+    dir: Direction,
+    count: usize
+}
+
+impl Instruction {
+    fn parse(line: &str) -> Instruction {
+        let fields: Vec<&str> = line.split(' ').collect();
+        let dir: Direction = Direction::parse(fields[0]);
+        let count: usize = fields[1].parse().unwrap();
+        Instruction { dir, count }
+    }
+
+    fn apply(&self, digger: &mut Digger, dug_area: &mut DugArea) {
+        for _ in 0 .. self.count {
+            digger.step(&self.dir);
+            dug_area.extend(digger.pos);
+        }
+    }
+}
+
+struct Digger {
+    pos: (isize, isize),
+    side_l: HashSet<(isize, isize)>,
+    side_r: HashSet<(isize, isize)>
+}
+
+impl Digger {
+    fn new() -> Digger {
+        Digger {
+            pos: (0, 0),
+            side_l: HashSet::new(),
+            side_r: HashSet::new()
+        }
+    }
+
+    fn step(&mut self, dir: &Direction) {
+        match dir {
+            Direction::North => {
+                self.side_l.insert((self.pos.0 - 1, self.pos.1 - 1));
+                self.side_r.insert((self.pos.0 - 1, self.pos.1 + 1));
+                self.pos.0 -= 1;
+            },
+            Direction::East  => {
+                self.side_l.insert((self.pos.0 - 1, self.pos.1 + 1));
+                self.side_r.insert((self.pos.0 + 1, self.pos.1 + 1));
+                self.pos.1 += 1;
+            },
+            Direction::South => {
+                self.side_l.insert((self.pos.0 + 1, self.pos.1 + 1));
+                self.side_r.insert((self.pos.0 + 1, self.pos.1 - 1));
+                self.pos.0 += 1;
+            },
+            Direction::West  => {
+                self.side_l.insert((self.pos.0 + 1, self.pos.1 - 1));
+                self.side_r.insert((self.pos.0 - 1, self.pos.1 - 1));
+                self.pos.1 -= 1;
+            },
+        }
+    }
+
+    fn valid_side(&self, dug_area: &DugArea) -> &HashSet<(isize, isize)> {
+        if self.side_l.iter().all(|pos| dug_area.contains(pos)) {
+            &self.side_l
+        } else {
+            &self.side_r
+        }
+    }
+}
+
+struct DugArea {
+    tiles: HashSet<(isize, isize)>,
+    min_row: isize,
+    max_row: isize,
+    min_col: isize,
+    max_col: isize
+}
+
+impl DugArea {
+    fn new() -> DugArea {
+        DugArea {
+            tiles: HashSet::from([(0, 0)]),
+            min_row: 0,
+            max_row: 0,
+            min_col: 0,
+            max_col: 0
+        }
+    }
+
+    fn extend(&mut self, pos: (isize, isize)) {
+        self.min_row = self.min_row.min(pos.0);
+        self.max_row = self.max_row.max(pos.0);
+        self.min_col = self.min_col.min(pos.1);
+        self.max_col = self.max_col.max(pos.1);
+        self.tiles.insert(pos);
+    }
+
+    fn grow(&mut self, inside: &HashSet<(isize, isize)>) {
+        let mut queue: Vec<(isize, isize)> = Vec::new();
+        let mut index: usize = 0;
+
+        for inside_tile in inside {
+            queue.push((inside_tile.0, inside_tile.1));
+        }
+
+        while index < queue.len() {
+            let pos: (isize, isize) = queue[index];
+
+            if !self.tiles.contains(&pos) {
+                queue.push((pos.0 - 1, pos.1));
+                queue.push((pos.0 + 1, pos.1));
+                queue.push((pos.0, pos.1 - 1));
+                queue.push((pos.0, pos.1 + 1));
+                self.tiles.insert(pos);
+            }
+
+            index += 1;
+        }
+    }
+
+    fn contains(&self, pos: &(isize, isize)) -> bool {
+        pos.0 >= self.min_row && pos.0 <= self.max_row && pos.1 >= self.min_col && pos.1 <= self.max_col
+    }
+}
+
 pub fn solve(lines: &Vec<String>) -> Solution {
-    println!("Input: {:?}", lines);
-    return Solution::Integer(0)
+    let instructions: Vec<Instruction> = lines.iter()
+        .map(|l| Instruction::parse(l)).collect();
+
+    let mut dug_area: DugArea = DugArea::new();
+    let mut digger: Digger = Digger::new();
+    
+    for instruction in instructions {
+        instruction.apply(&mut digger, &mut dug_area);
+    }
+
+    let inside: &HashSet<(isize, isize)> = digger.valid_side(&dug_area);
+    
+    dug_area.grow(inside);
+
+    let result: i64 = dug_area.tiles.len() as i64;
+    return Solution::Integer(result)
 }
